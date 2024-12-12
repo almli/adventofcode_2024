f6b43a195affd43c08b31d06ceb5ca444ee8b490
Lars Ivar Almli <lars.ivar.almli@sparebank1.no>
2024-12-12 18:21:38 +0100

    Dag 12 del 2

--- src/main/kotlin/dag12/del2/Main.kt
+++ src/main/kotlin/dag12/del2/Main.kt
@@ -1,6 +1,7 @@
 package dag12.del2
+
 import java.io.File
- 
+
 lateinit var data: Array<CharArray>
 val found = mutableSetOf<Pair<Int, Int>>()
 
@@ -12,59 +13,66 @@ fun main() {
         for (j in data[i].indices) {
             if (found.contains(Pair(j, i))) continue
             val currentRegion = mutableSetOf<Pair<Int, Int>>()
-            val borders = findRegionFrom(data[i][j], Pair(j, i), currentRegion,"X")
-            println(data[i][j] + ":H:"+ splitByHolesInSequence(borders, "H") + ":V:"+ splitByHolesInSequence(borders, "V"))
+            val borders = findRegionFrom(data[i][j], Pair(j, i), currentRegion, 'R')
             found.addAll(currentRegion)
-            sum += (splitByHolesInSequence(borders, "H").size + splitByHolesInSequence(borders, "V").size) * currentRegion.size
+            sum += (borderCount(borders) * currentRegion.size)
         }
     }
     println(sum)
 }
 
-fun findRegionFrom(type: Char,  square: Pair<Int, Int>, currentRegion: MutableSet<Pair<Int, Int>>, dir: String ): List<Pair<String, Pair<Int, Int>>> {
+fun borderCount(borders: List<Pair<Char, Pair<Int, Int>>>): Int {
+    var count = 0
+    for (dirBorders in borders.groupBy({ it.first }, { it.second })) {
+        if (dirBorders.key == 'R' || dirBorders.key == 'L') {
+            count += countVerticalBorders(dirBorders.value)
+        } else {
+            count += countHorizontalBorders(dirBorders.value)
+        }
+    }
+    return count;
+}
+
+fun countHorizontalBorders(borders: List<Pair<Int, Int>>): Int {
+    return borders
+        .groupBy({ it.second }, { it.first })
+        .values
+        .sumOf { segmentCount(it) }
+}
+
+fun countVerticalBorders(borders: List<Pair<Int, Int>>): Int {
+    return borders
+        .groupBy({ it.first }, { it.second })
+        .values
+        .sumOf { segmentCount(it) }
+}
+
+fun segmentCount(ints: List<Int>): Int {
+    if (ints.isEmpty()) return 0
+
+    return ints
+        .sorted()
+        .zipWithNext()
+        .count { it.second != it.first + 1 } + 1
+}
+
+fun findRegionFrom(
+    type: Char,
+    square: Pair<Int, Int>,
+    currentRegion: MutableSet<Pair<Int, Int>>,
+    dir: Char
+): List<Pair<Char, Pair<Int, Int>>> {
     if (currentRegion.contains(square)) return emptyList()
     if (!isSameRegion(type, square)) return listOf(Pair(dir, square))
     currentRegion.add(square)
 
-    return findRegionFrom(type, Pair(square.first + 1, square.second), currentRegion, "H") +
-           findRegionFrom(type, Pair(square.first - 1, square.second), currentRegion, "H") +
-           findRegionFrom(type, Pair(square.first, square.second + 1), currentRegion, "V") +
-           findRegionFrom(type, Pair(square.first, square.second - 1), currentRegion, "V")
+    return findRegionFrom(type, Pair(square.first + 1, square.second), currentRegion, 'R') +
+            findRegionFrom(type, Pair(square.first - 1, square.second), currentRegion, 'L') +
+            findRegionFrom(type, Pair(square.first, square.second + 1), currentRegion, 'D') +
+            findRegionFrom(type, Pair(square.first, square.second - 1), currentRegion, 'U')
 }
 
 fun isSameRegion(type: Char, square: Pair<Int, Int>): Boolean {
     val (x, y) = square
     return x >= 0 && y >= 0 && y < data.size && x < data[y].size && data[y][x] == type
 }
-fun splitByHolesInSequence(borders: List<Pair<String, Pair<Int, Int>>>, dir: String): Map<Int, List<List<Int>>> {
-    return borders
-        .filter { it.first == dir }
-        .groupBy { if (dir == "H") it.second.first else it.second.second } // Group by second for "H", first for "V"
-        .mapValues { (_, group) ->
-            group.map { if (dir == "H") it.second.second else it.second.first }
-                .distinct()
-                .sorted()
-                .splitByHoles()
-        }
-}
-
-private fun List<Int>.splitByHoles(): List<List<Int>> {
-    if (isEmpty()) return emptyList()
-
-    val result = mutableListOf<MutableList<Int>>()
-    var currentGroup = mutableListOf<Int>()
-    currentGroup.add(this.first())
-
-    for (i in 1 until this.size) {
-        if (this[i] == this[i - 1] + 1) {
-            currentGroup.add(this[i])
-        } else {
-            result.add(currentGroup)
-            currentGroup = mutableListOf(this[i])
-        }
-    }
-    result.add(currentGroup)
-
-    return result
-}
-
